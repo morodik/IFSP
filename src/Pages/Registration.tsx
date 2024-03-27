56bab65da061c530348ce529038190decd50fdbf
morodik <sashamoroz0412@gmail.com>
2024-03-27 13:07:03 +0300

    second commit

--- src/Pages/Registration.tsx
+++ src/Pages/Registration.tsx
@@ -15,9 +15,9 @@ function Registration() {
                     <h2 id='regtxt'>Регистрация</h2>
                     <h2 id='logintxt'>Логин:</h2>
                     <input id='login'></input>
-                    <h2 id='passtxt'>Придумайте пароль:</h2>
+                    <h2 id='passtxt'>Придумайте пароль</h2>
                     <input id='pass' type='password'></input>
-                    <h2 id='rpasstxt'>Повторите пароль:</h2>
+                    <h2 id='rpasstxt'>Повторите пароль</h2>
                     <input id='rpass' type='password'></input>
                     <button id='reg' type='submit'>Зарегестрироваться</button>
                 </form>

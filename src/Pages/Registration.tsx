7d5ef041598b297c3080d444f8c48fff545ebdf8
morodik <sashamoroz0412@gmail.com>
2024-03-30 21:03:56 +0300

    Валидация Регистрации

--- src/Pages/Registration.tsx
+++ src/Pages/Registration.tsx
@@ -9,12 +9,14 @@ function Registration() {
     const [email, setEmail] = useState<string | undefined>();
     const [dirtyEmail, setDirtyEmail] = useState(false);
     const [emailError, setEmailError] = useState('Поле не может быть пустым');
-    const [formValid, setFormValid] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState<string | undefined>();
+    const [dirtyConfirmPassword, setDirtyConfirmPassword] = useState(false);
+    const [confirmPasswordError, setConfirmPasswordError] = useState('Поле не может быть пустым');
 
     const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
-        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        if (!re.test(String(e.target.value).toLowerCase())) {
+        const re =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(!re.test(String(e.target.value).toLowerCase())) {
             setEmailError('Некорректный логин')
         } else {
             setEmailError('')
@@ -31,30 +33,28 @@ function Registration() {
             setPasswordError('');
         }
     };
-
+    const confirmPasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setConfirmPassword(e.target.value);
+        if (!e.target.value) {
+            setConfirmPasswordError('Поле не может быть пустым')
+        } else if (e.target.value != password) {
+            setConfirmPasswordError('Пароли не совпадают')
+        } else {
+            setConfirmPasswordError('');
+        } 
+    };
     const blurHandler = (e: React.FocusEvent<HTMLInputElement, Element>) => {
         switch (e.target.name) {
             case 'email':
                 setDirtyEmail(true)
-                break;
+                break
             case 'password':
                 setDirtyPassword(true)
-                break;
+                break
             case 'rpassword':
-                break;
+                setDirtyConfirmPassword(true)
         }
     };
-
-    // Проверка валидности формы
-    useEffect(() => {
-        if (emailError || passwordError) {
-            setFormValid(false);
-        } else {
-            setFormValid(true);
-        }
-    }, [emailError, passwordError]);
-
-
     return (
         <div>
             <div className='susa'>
@@ -66,25 +66,38 @@ function Registration() {
                 <form id='rectangle36'>
                     <h2 id='regtxt'>Регистрация</h2>
                     <h2 id='logintxt'>Логин:</h2>
+                    <div>
                     <input 
                         value={email}
                         onChange={e => emailHandler(e)}
-                        onBlur={e => blurHandler(e)}
+                        onBlur={e => blurHandler(e)} name="email"
+                        style={{borderColor: dirtyEmail && emailError ? 'rgba(210, 94, 94, 1)' : 'rgba(128, 94, 210, 1)' }}
                         id='login'></input>
-                     {((dirtyEmail && emailError)) && <div className='error_message_email' >{emailError}</div>}
+                    </div>
+                    {((dirtyEmail && emailError)) && <div id='error_message_email' >{emailError}</div>}
                     <h2 id='passtxt'>Придумайте пароль</h2>
-                    <input
-                        value={password} 
-                        onChange={e => passwordHandler(e)}
-                        onBlur={e => blurHandler(e)}
-                        name='password' id='pass' type='password'></input>
-                    {((dirtyPassword && passwordError)) && <div className='error_message' >{passwordError}</div>}
+                    <div>
+                        <input
+                            onChange={e => passwordHandler(e)}
+                            onBlur={e => blurHandler(e)} name="password"
+                            id='pass' type='password'
+                            style={{borderColor: dirtyPassword && passwordError ? 'rgba(210, 94, 94, 1)' : 'rgba(128, 94, 210, 1)'}}></input>
+                            {((dirtyPassword && passwordError)) && <div id='error_message' >{passwordError}</div>}
+                        </div>
                     <h2 id='rpasstxt'>Повторите пароль</h2>
-                    <input 
-                        value={password}
-                        onBlur={e => blurHandler(e)}
-                        name='rpassword' id='rpass' type='password'></input>
-                    <button disabled={!formValid} id='reg' type='submit'>Зарегистрироваться</button>
+                    <div>
+                    <input
+                            value={confirmPassword}
+                            onChange={e => confirmPasswordHandler(e)}
+                            onBlur={e => blurHandler(e)}
+                            name='rpassword'
+                            id='rpass'
+                            type='password'
+                            style={{ borderColor: dirtyConfirmPassword && confirmPasswordError ? 'rgba(210, 94, 94, 1)' : 'rgba(128, 94, 210, 1)' }}
+                        />
+                        {dirtyConfirmPassword && confirmPasswordError && <div id='error_message_confirm'>{confirmPasswordError}</div>}
+                    </div>
+                    <button id='reg' type='submit'>Зарегистрироваться</button>
                 </form>
             </div>
         </div>

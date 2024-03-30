6114228ea158d7a220f19e429e6bcd0dc569bb79
morodik <sashamoroz0412@gmail.com>
2024-03-30 12:43:22 +0300

    Валидация

--- src/Pages/Enter.tsx
+++ src/Pages/Enter.tsx
@@ -1,15 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Enter.css';
 import NavBar from '../UI/NavBar';
 import { Link } from 'react-router-dom';
 
 function Enter() {
-    const [password, setPassword] = useState();
+    const [password, setPassword] = useState<string | undefined>();
     const [dirtyPassword, setDirtyPassword] = useState(false);
-    const [passwordError, setPasswordError] = useState('Поля не могу быть пустыми');
-    const [email, setEmail] = useState();
+    const [passwordError, setPasswordError] = useState('Поле не может быть пустым' );
+    const [email, setEmail] = useState<string | undefined>();
     const [dirtyEmail, setDirtyEmail] = useState(false);
-    const [emailError, setEmailError] = useState('Поля не могут быть пустыми');
+    const [emailError, setEmailError] = useState('Поле не может быть пустым');
+    const [formValid, setFormValid] = useState(false);
+
+    useEffect(() => {
+        if (emailError || passwordError) {
+            setFormValid(false);
+        } else {
+            setFormValid(true);
+        }
+    }, [emailError, passwordError])
+
+    const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+        const re =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if(!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Некорректный логин')
+        } else {
+            setEmailError('')
+        }
+    };
+
+    const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+        if (!e.target.value) {
+            setPasswordError('Поле не может быть пустым');
+        } else if (e.target.value.length < 3) {
+            setPasswordError('Минимальное количество символов - 3');
+        } else {
+            setPasswordError('');
+        }
+    };
 
     const blurHandler = (e: React.FocusEvent<HTMLInputElement, Element>) => {
         switch (e.target.name) {
@@ -20,7 +50,7 @@ function Enter() {
                 setDirtyPassword(true)
                 break
         }
-    }
+    };
     
     return (
         <div className='container'>
@@ -31,13 +61,26 @@ function Enter() {
                         <h2 className='txth2'> Вход</h2>
                     </div>
                     <div id='loginInp'>
-                        <input onBlur={e => blurHandler(e)} name="email" className="input" type="email" placeholder="Логин"></input>
+                        <input value={email}
+                        onChange={e => emailHandler(e)}
+                        onBlur={e => blurHandler(e)} name="email" 
+                        className="input" type="email" placeholder="Логин"
+                        style={{borderColor: dirtyEmail && emailError ? 'rgba(210, 94, 94, 1)' : 'rgba(128, 94, 210, 1)'}}>
+
+                        </input>
                     </div>
+                    {((dirtyEmail && emailError)) && <div className='error_message_email' >{emailError}</div>}
                     <div id='passInp'>
-                        <input onBlur={e => blurHandler(e)} name="password" id="inputPass" type="password" placeholder="Пароль"></input>
-                        {((dirtyEmail && emailError) || (dirtyPassword && passwordError)) && <div style={{color: 'rgba(210, 94, 94, 1)'}}>{passwordError}</div>}
+                        <input value={password} 
+                        onChange={e => passwordHandler(e)}
+                        onBlur={e => blurHandler(e)} name="password" id="inputPass" 
+                        type="password" placeholder="Пароль"
+                        style={{borderColor: dirtyPassword && passwordError ? 'rgba(210, 94, 94, 1)' : 'rgba(128, 94, 210, 1)'}}>
+        
+                        </input>
                     </div>
-                    <button className='subm'type='submit'>Войти</button>
+                    {((dirtyPassword && passwordError)) && <div className='error_message' >{passwordError}</div>}
+                    <button disabled={!formValid} className={formValid ? 'subm' : 'subm disabled'} type='submit'>Войти</button>
                     <Link to='/registration' className='regTo'>Регистрация</Link>
 
                 </form>
--- src/Pages/Registration.tsx
+++ src/Pages/Registration.tsx
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './Registration.css';
 
 function Registration() {
-    
+    const [password, setPassword] = useState<string | undefined>();
+    const [dirtyPassword, setDirtyPassword] = useState(false);
+    const [passwordError, setPasswordError] = useState('Поле не может быть пустым' );
+    const [email, setEmail] = useState<string | undefined>();
+    const [dirtyEmail, setDirtyEmail] = useState(false);
+    const [emailError, setEmailError] = useState('Поле не может быть пустым');
+    const [formValid, setFormValid] = useState(false);
+
+    const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Некорректный логин')
+        } else {
+            setEmailError('')
+        }
+    };
+
+    const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+        if (!e.target.value) {
+            setPasswordError('Поле не может быть пустым');
+        } else if (e.target.value.length < 3) {
+            setPasswordError('Минимальное количество символов - 3');
+        } else {
+            setPasswordError('');
+        }
+    };
+
+    const blurHandler = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+        switch (e.target.name) {
+            case 'email':
+                setDirtyEmail(true)
+                break;
+            case 'password':
+                setDirtyPassword(true)
+                break;
+            case 'rpassword':
+                break;
+        }
+    };
+
+    // Проверка валидности формы
+    useEffect(() => {
+        if (emailError || passwordError) {
+            setFormValid(false);
+        } else {
+            setFormValid(true);
+        }
+    }, [emailError, passwordError]);
+
+
     return (
         <div>
             <div className='susa'>
@@ -14,16 +66,30 @@ function Registration() {
                 <form id='rectangle36'>
                     <h2 id='regtxt'>Регистрация</h2>
                     <h2 id='logintxt'>Логин:</h2>
-                    <input id='login'></input>
+                    <input 
+                        value={email}
+                        onChange={e => emailHandler(e)}
+                        onBlur={e => blurHandler(e)}
+                        id='login'></input>
+                     {((dirtyEmail && emailError)) && <div className='error_message_email' >{emailError}</div>}
                     <h2 id='passtxt'>Придумайте пароль</h2>
-                    <input id='pass' type='password'></input>
+                    <input
+                        value={password} 
+                        onChange={e => passwordHandler(e)}
+                        onBlur={e => blurHandler(e)}
+                        name='password' id='pass' type='password'></input>
+                    {((dirtyPassword && passwordError)) && <div className='error_message' >{passwordError}</div>}
                     <h2 id='rpasstxt'>Повторите пароль</h2>
-                    <input id='rpass' type='password'></input>
-                    <button id='reg' type='submit'>Зарегестрироваться</button>
+                    <input 
+                        value={password}
+                        onBlur={e => blurHandler(e)}
+                        name='rpassword' id='rpass' type='password'></input>
+                    <button disabled={!formValid} id='reg' type='submit'>Зарегистрироваться</button>
                 </form>
             </div>
         </div>
     )
-}
+};
+
 
 export default Registration;
